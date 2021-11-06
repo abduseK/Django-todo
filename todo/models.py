67a60db1e2a32489e9574f81fbca973aa725a8d7
abdelselam kemal <abdelselamkemal74@gmail.com>
2021-11-06 12:27:55 -0700

    init commit

---
+++ todo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    date_created = models.DateTimeField()
